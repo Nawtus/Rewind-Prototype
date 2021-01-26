4250a60c8b88c29a5ab710d72c161edc7f8e3f15
Nawtus <60529165+Nawtus@users.noreply.github.com>
2021-01-26 10:01:17 -0300

    Commit initial

---
+++ Assets/Scripts/Player/MovementScript.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    [Header("Movement")]
+    private Rigidbody2D rig;
+    [SerializeField] float horizontal;
+    [SerializeField] float vertical;
+    [SerializeField] bool isLookLeft;
+    [SerializeField] float speed;
+    public float Speed { get => speed; set => speed = value; }
+
+    void Start()
+    {
+        //Movement
+        rig = GetComponent<Rigidbody2D>();
+        Speed = 5f;
+    }
+
+
+
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+
+
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        //vertical = Input.GetAxisRaw("Vertical");
+
+        if (horizontal > 0 && isLookLeft == true)
+        {
+            Flip();
+        }
+        else if (horizontal < 0 && isLookLeft == false)
+        {
+            Flip();
+        }
+    }
+
+
+
+    void Movement()
+    {
+        if (horizontal != 0 /*&& vertical != 0*/)
+        {
+            horizontal *= 0.75f;
+            //vertical *= 0.75f;
+        }
+
+        rig.velocity = new Vector2(horizontal * Speed, rig.velocity.y);
+
+        /*if (horizontal != 0 || vertical != 0)
+        {
+            anim.SetBool("Walking", true);
+        }
+        else
+        {
+            anim.SetBool("Walking", false);
+        }*/
+    }
+
+
+
+    void Flip()
+    {
+        if (isLookLeft == true)
+        {
+            isLookLeft = !isLookLeft;
+            transform.Rotate(0, 180, 0);
+        }
+        else if (isLookLeft == false)
+        {
+            isLookLeft = !isLookLeft;
+            transform.Rotate(0, 180, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Rewind.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rewind : MonoBehaviour
+{
+    public bool isRewinding;
+    public List<Vector3> positions = new List<Vector3>(500); 
+    public MovementScript movement;
+
+
+
+    private void FixedUpdate()
+    {
+        if (!isRewinding)
+        {
+            SavePositions();
+            movement.enabled = true;
+        }
+        else
+        {
+            movement.enabled = false;
+        }
+    }
+
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(RewindToLastPosition());
+        }
+    }
+
+
+
+    void SavePositions()
+    {
+        if (positions.Count >= positions.Capacity)
+        {
+            for (int i = 0; i < positions.Count; i++)
+            {
+                positions.Remove(positions[0]);
+            }
+        }
+        else
+        {
+            positions.Add(transform.position);
+        }
+    }
+
+
+
+    private IEnumerator RewindToLastPosition()
+    {
+        isRewinding = true;
+        for (int i = positions.Count - 1; i >= 0; i--)
+        {
+            transform.position = Vector3.Lerp(transform.position, positions[i], movement.Speed * Time.deltaTime);
+            positions.Remove(positions[i]);
+            yield return new WaitForEndOfFrame();
+        }
+        isRewinding = false;
+    }
+}
